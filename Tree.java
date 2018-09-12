9a9bb4d11b9e14d4fe868b472e7391491bb6e15c
Tabbakk <38476904+Tabbakk@users.noreply.github.com>
2018-09-12 17:11:13 -0600

    Update Tree.java
    Added input from console and fixed bugs occurring when indentation was given or taken away without other chars present (ie: ",]," )

--- Tree.java
+++ Tree.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Tree {
-	static String input = "[1, 2, [A, B, C, [5^&, )()6, , 7], D, E], 3, 4]";
-	
+
 	//I have made the assumption that all values must be separated by a comma, last element excluded. Moreover, an element can receive or be detracted of multiple indentations using multiple '[' or ']' on a single element (but there can not be spaces between the multiple parenthesis or it is considered part of the string 
 	
 	public static void main(String[] args){
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		// Reading data using readLine
+		System.out.print("Insert pattern: ");
+		String input = "";
+		try {
+			input = reader.readLine();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+			
 		if(verifyTree(input)) {
 			printTree(input);
 		}
@@ -14,7 +30,7 @@ public class Tree {
 	
 	// This method prints the given tree on screen, if it has been verified to be an acceptable input
 	public static void printTree(String input) {
-
+		
 		input=input.trim();
 		String[] elements = input.split(","); // O(n) where n is length of string
 		boolean first = true;
@@ -24,17 +40,29 @@ public class Tree {
 			String element = elements[i].trim(); //Removal of space before and after the element
 			if(element.length()>0) { //I proceed to value a possible change of indentation of an element only if the element is not an empty space
 				while(element.charAt(0) == '[') { //trimming of prefixed '['s and consequential increment of indentation
-					element = element.substring(1);
 					if(first) {
 						first=false;
 					}
 					else {
 						spaces = spaces+"    ";
 					}
+					if(element.length()>1) { //verifying length of string prior to executing substring
+						element = element.substring(1);
+					}
+					else {
+						element = "";
+						break;
+					}
 				}
-				while(element.charAt(element.length()-1) == ']') { //trimming of suffixed ']'s and consequential marking for decrement of indentation
-					element = element.substring(0, element.length()-1);
+				while(element.length()>0 && element.charAt(element.length()-1) == ']') { //trimming of suffixed ']'s and consequential marking for decrement of indentation
 					removeIndent++;
+					if(element.length()>1) { //verifying length of string prior to executing substring
+						element = element.substring(0, element.length()-1);
+					}
+					else {
+						element="";
+						break;
+					}
 				}
 				System.out.println(spaces+element);
 				if(removeIndent>0 && i<elements.length-1) { //decrement of indentation for a total of removeIndent levels
@@ -50,11 +78,10 @@ public class Tree {
 		
 	}
 	
-	// this method verifies that the input is formatted as to form an acceptable tree
+	// This method verifies if the formatting of the given string can correctly forms a tree
 	public static Boolean verifyTree(String given) {
-		
 		given=given.trim();
-		if(given.charAt(0)=='[' && given.charAt(given.length()-1)==']') {   //If the first and last chars of the string don't open and close the structure, it is invalid. 
+		if(given.charAt(0)=='[' && given.trim().charAt(given.length()-1)==']') {   //If the first and last chars of the string don't open and close the structure, it is invalid. 
 			String[] elements = given.split(","); //O(n) where n is the length of the string.
 			int verify = 0;
 			int length = elements.length-1;
@@ -63,8 +90,22 @@ public class Tree {
 				if (elements[i].trim().length() > 0) {
 					int top = 0;
 					int bottom = elements[i].trim().length()-1;
-					while (elements[i].trim().charAt(top)=='[') {verify++; top++;} // increments for every opened [
-					while (elements[i].trim().charAt(bottom)==']') {verify--;bottom--;} // decrements for every closed ]
+					while (elements[i].trim().charAt(top)=='[') {
+						if(elements[i].trim().length()-top==1) { //verifying char top is pointing to prior incrementing it
+							verify++;
+							break;
+						}
+						verify++; 
+						top++;
+					} // increments for every opened [
+					while (elements[i].trim().charAt(bottom)==']') {
+						if(elements[i].trim().length()-bottom==elements[i].trim().length()) { //verifying char bottom is pointing to prior decrementing it
+							verify--;
+							break;
+						}
+						verify--;
+						bottom--;
+					} // decrements for every closed ]
 				}
 				j=i;
 				if (verify<0) {
@@ -86,4 +127,5 @@ public class Tree {
 		}
 		
 	}
+
 }
